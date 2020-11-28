c204a0610a2ed9e10d5f6b31a856d7e979af25c8
Youdi123 <71049753+Youdi123@users.noreply.github.com>
2020-11-27 19:47:13 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-var images = [
-"chicago1.jpg",
-"chicago2.jpg",
-"chicago3.jpg",
-"chicago4.jpg"
-];
-var i=0;
-function slide() {
-  document.getElementById("first-image").src=images[i];
-  if(i<(images.length-1))
-    i++;
-  else
-    i=0;
-}
-setInterval(slide,1000) 
+
